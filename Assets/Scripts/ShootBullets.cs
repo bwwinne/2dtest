533267724c6b3c56fc6cbe5d7a90c015148f92a6
Bryce <bwwinne@gmail.com>
2019-11-24 18:19:37 -0500

    buncha stuff

---
+++ Assets/Scripts/ShootBullets.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ShootBullets : NetworkBehaviour
+{
+    [SerializeField]
+    private GameObject bulletPrefab;
+
+    [SerializeField]
+    private float bulletSpeed;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("space");
+        }
+        if (this.isLocalPlayer && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("shoot");
+            this.CmdShoot();
+        }
+    }
+
+    [Command]
+    void CmdShoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, this.transform.position, Quaternion.identity);
+        bullet.GetComponent<Rigidbody2D>().velocity = Vector2.up * bulletSpeed;
+        NetworkServer.Spawn(bullet);
+        Destroy(bullet, 1.0f);
+    }
+}
